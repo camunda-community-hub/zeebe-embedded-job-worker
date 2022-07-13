f4461a102f198abbb29ef295e3f5224e0edd07ee
Falko Menge <falko.menge@camunda.com>
2022-07-13 15:43:22 +0200

    Fix format

--- src/main/java/org/camunda/community/extension/zeebe/exporter/jobworker/EmbeddedJobWorker.java
+++ src/main/java/org/camunda/community/extension/zeebe/exporter/jobworker/EmbeddedJobWorker.java
@@ -1,7 +1,5 @@
 package org.camunda.community.extension.zeebe.exporter.jobworker;
 
-import java.util.Set;
-
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.exporter.api.Exporter;
 import io.camunda.zeebe.exporter.api.context.Context;
@@ -10,51 +8,51 @@ import io.camunda.zeebe.exporter.api.context.Controller;
 import io.camunda.zeebe.protocol.record.RecordType;
 import io.camunda.zeebe.protocol.record.ValueType;
 import io.camunda.zeebe.protocol.record.intent.JobIntent;
+import java.util.Set;
 
 public class EmbeddedJobWorker implements Exporter {
 
-    private Controller controller;
-    private ZeebeClient client;
+  private Controller controller;
+  private ZeebeClient client;
 
-    @Override
-    public void configure(final Context context) throws Exception {
-      context.setFilter(
-          new RecordFilter() {
-            private static final Set<ValueType> ACCEPTED_VALUE_TYPES =
-                Set.of(ValueType.JOB);
-  
-            @Override
-            public boolean acceptType(final RecordType recordType) {
-              return recordType == RecordType.EVENT;
-            }
-  
-            @Override
-            public boolean acceptValue(final ValueType valueType) {
-              return ACCEPTED_VALUE_TYPES.contains(valueType);
-            }
-          });
+  @Override
+  public void configure(final Context context) throws Exception {
+    context.setFilter(
+        new RecordFilter() {
+          private static final Set<ValueType> ACCEPTED_VALUE_TYPES = Set.of(ValueType.JOB);
+
+          @Override
+          public boolean acceptType(final RecordType recordType) {
+            return recordType == RecordType.EVENT;
+          }
+
+          @Override
+          public boolean acceptValue(final ValueType valueType) {
+            return ACCEPTED_VALUE_TYPES.contains(valueType);
+          }
+        });
+  }
+
+  @Override
+  public void open(Controller controller) {
+    this.controller = controller;
+    this.client =
+        ZeebeClient.newClientBuilder()
+            .usePlaintext()
+            .gatewayAddress("camunda-zeebe-gateway:26500")
+            .build();
+  }
+
+  @Override
+  public void close() {
+    client.close();
+  }
+
+  @Override
+  public void export(io.camunda.zeebe.protocol.record.Record<?> record) {
+    if (record.getIntent() == JobIntent.CREATED) {
+      client.newCompleteCommand(record.getKey()).send();
     }
-
-    @Override
-    public void open(Controller controller) {
-        this.controller = controller;
-        this.client = ZeebeClient.newClientBuilder()
-          .usePlaintext()
-          .gatewayAddress("camunda-zeebe-gateway:26500")
-          .build();
-    }
-
-    @Override
-    public void close() {
-      client.close();
-    }
-
-    @Override
-    public void export(io.camunda.zeebe.protocol.record.Record<?> record) {
-        if (record.getIntent() == JobIntent.CREATED) {
-          client.newCompleteCommand(record.getKey()).send();
-        }
-        this.controller.updateLastExportedRecordPosition(record.getPosition());
-    }
-
+    this.controller.updateLastExportedRecordPosition(record.getPosition());
+  }
 }
